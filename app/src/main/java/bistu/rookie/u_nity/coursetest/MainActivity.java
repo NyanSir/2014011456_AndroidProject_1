cc6d1789a77499dd4f320560b3ad89f2622aef9a
U-nity <U-nity@U-nity-2.local>
2016-09-05 12:08:09 +0800

    Initial commit

---
+++ app/src/main/java/bistu/rookie/u_nity/coursetest/MainActivity.java
@@ -0,0 +1,15 @@
+package bistu.rookie.u_nity.coursetest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //"Hello World!" in layout <TextView>
+    }
+}
