bfce50e18657a14eef931524c96b7689180586dd
U-nity <728501875@qq.com>
2016-09-05 12:38:14 +0800

    Hello Github!

--- app/src/main/java/bistu/rookie/u_nity/coursetest/MainActivity.java
+++ app/src/main/java/bistu/rookie/u_nity/coursetest/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //"Hello World!" in layout <TextView>
+        //"Hello World!" in layout <TextView> which displays on the screen.
     }
 }
